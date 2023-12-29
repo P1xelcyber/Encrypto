654774f1645c5242db5c3b9db208c1f851c48399
P1xelcyber <149936014+P1xelcyber@users.noreply.github.com>
2023-12-29 13:17:54 -0500

    Update Encrypto++.py

--- Encrypto++.py
+++ Encrypto++.py
@@ -1,127 +1,171 @@
-from tkinter import filedialog as fd
-import tkinter as tk
-from tkinter import *
+#GITHUB:P1xelcyber
 from cryptography.fernet import Fernet
-import os
+from tkinter import filedialog as fd
 from tkinter import filedialog
+from tkinter import *
+import tkinter as tk
+import tkinter.messagebox 
+import os
+import base64
 
 
-root = Tk()
-root.title("Encrypto++")
-root.geometry("670x400")
-root.resizable(False, False)
-root.configure(bg="black")
-
-
-
-key_entry = tk.Entry(root,bg="grey", fg="green",width="50")
-key_entry.place(x=35, y=150)
-
-def select_key():
-    fp = fd.askopenfilename()
-    with open(fp, "rb") as file:
-        key = file.read().decode('utf-8')
-    key_entry.delete(0, "end")
-    key_entry.insert(0, key)
-
-def path_selection():
-    fp2 = filedialog.askdirectory()
-    files = []
-    for file in os.listdir(fp2):
-        if file == "Encrypto++.py":
-            continue
-        files.append(file)
-    label4.config(text=files)
-
-def folder_path_selection():
-    fp2 = filedialog.askdirectory()
-    label4.config(text=fp2, fg="green")
-    
-def file_path_selection():
-    fp3 = fd.askopenfilename()
-    label4.config(fg="green", text=fp3)
+root = tk.Tk()
+entry = Entry(root,width=45)
+entry1 = Entry(root, width=45)
 
 def generate_key():
-    key = Fernet.generate_key()
-    key_entry.delete(0,"end")
-    key_entry.insert(0,key)
+    key1 = Fernet.generate_key()
+    entry.config(foreground='black')
+    entry.delete(0, 'end')
+    entry.insert(0, key1)
 
+def decrypt():
+    path = entry1.get()
+    key = entry.get()
+    f = Fernet(key)
+    if os.path.isfile(path):
+        with open(path, "rb") as file:
+            content = file.read()
+        decrypted_content = f.decrypt(content)
+        with open(path, "wb") as file:
+            file.write(decrypted_content)
+    elif os.path.isdir(path):
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                patho = os.path.join(root, file)
+                with open(patho, "rb") as file:
+                    content = file.read()
+                decrypted_content = f.decrypt(content)
+                with open(patho, "wb") as file:
+                    file.write(decrypted_content)
 
-def save_key_to_file():
-    key = key_entry.get().encode('utf-8')
-    with open("file.key", "wb") as file:
-        file.write(key)
-
+    else:
+        tkinter.messagebox.showinfo("Error",  "Error code 342\nError fixes on github:p1xelcyber") 
 
 def encrypt():
-    file_path = label4.cget("text")
-    with open(file_path, 'rb') as file:
-        original = file.read()
+    path = entry1.get()
+    key = entry.get()
+    f = Fernet(key)
+    if os.path.isfile(path):
+        with open(path, "rb") as file:
+            content = file.read()
+        encrypted_content = f.encrypt(content)
+        with open(path, "wb") as file:
+            file.write(encrypted_content)
+    elif os.path.isdir(path):
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                patho = os.path.join(root, file)
+                with open(patho, "rb") as file:
+                    content = file.read()
+                encrypted_content = f.encrypt(content)
+                with open(patho, "wb") as file:
+                    file.write(encrypted_content)
+    else:
+        tkinter.messagebox.showinfo("Error",  "Error code 342\nError fixes on github:p1xelcyber") 
 
-    key = key_entry.get()
-    fernet = Fernet(key)
-    encrypted = fernet.encrypt(original)
+def folder_path():
+    fp3 = filedialog.askdirectory()
+    entry1.config(foreground='black')
+    entry1.delete(0, 'end')
+    entry1.insert(0, fp3)
 
-    with open(file_path, 'wb') as file:
-        file.write(encrypted)
+def file_path():
+    fp2 = filedialog.askopenfilename()
+    entry1.config(foreground='black')
+    entry1.delete(0, 'end')
+    entry1.insert(0, fp2)
 
-def dencrypt():
-    file_path = label4.cget("text")
-    with open(file_path, 'rb') as file:
-        original = file.read()
+def Load_key():
+    entry.config(foreground='black')
+    fp = filedialog.askopenfilename()
+    with open(fp, "rb") as file:
+        key = file.read().decode('utf-8')
+    entry.delete(0, 'end')
+    entry.insert(0, key)
 
-    key = key_entry.get()
-    fernet = Fernet(key)
-    decrypted = fernet.decrypt(original)
+def save_key():
+    key = entry.get().encode('utf-8')
+    with open("key.key", "wb") as file:
+        file.write(key)
+    tkinter.messagebox.showinfo("successful!",  "Key is saved to key.key") 
 
-    with open(file_path, 'wb') as file:
-        file.write(decrypted)
+def on_entry_click(event):
+    if entry.get() == 'Enter your key or generate one...':
+       entry.delete(0, "end")
+       entry.insert(0, '')
+       entry.config(fg = 'black')
 
+def on_focusout(event):
+    if entry.get() == '':
+        entry.insert(0, 'Enter your key or generate one...')
+        entry.config(fg = 'grey')
 
-load_button = tk.Button(root, text="Load Key", command=select_key, state="normal", borderwidth=0, bg="black", fg="green", activebackground='#2e2e2e', activeforeground="green")
-load_button.place(x=359, y=180)
+#GITHUB:P1xelcyber
+def on_entry_click1(event):
+    if entry1.get() == 'Enter your key or generate one...':
+       entry1.delete(0, 'end')
+       entry1.insert(0, '')
+       entry1.config(fg = 'black')
 
-decrypt_button = tk.Button(root, text="Decrypt", command=dencrypt, state="normal", borderwidth=0, bg="black", fg="green", activebackground='#2e2e2e', activeforeground="green")
-decrypt_button.place(x=365, y=310)
-
-save_button = tk.Button(root, command=save_key_to_file,text="Save Key", state="normal", borderwidth=0, bg="black", fg="green", activebackground='#2e2e2e', activeforeground="green")
-save_button.place(x=270, y=180)
-
-save_button = tk.Button(root, command=encrypt,text="Encrypt", state="normal", borderwidth=0, bg="black", fg="green", activebackground='#2e2e2e', activeforeground="green")
-save_button.place(x=284, y=310)
-
-save_button2 = tk.Button(root,command=file_path_selection,text="Select file", state="normal", borderwidth=0, bg="black", fg="green", activebackground='#2e2e2e', activeforeground="green")
-save_button2.place(x=35, y=310)
-
-file_button2 = tk.Button(root,command=folder_path_selection,text="Select folder", state="normal", borderwidth=0, bg="black", fg="green", activebackground='#2e2e2e', activeforeground="green")
-file_button2.place(x=131, y=310)
-
-label = tk.Label(text="Encrypto++", foreground="green", bg="black", borderwidth=0)
-label.config(font=("handjet", 50))
-label.place(x=30, y=0)
-
-label2 = tk.Label(text="Key⇩", foreground="green", bg="black", borderwidth=0)
-label2.config(font=("handjet", 25))
-label2.place(x=35, y=95)
-
-label3 = tk.Label(text="Path⇩", foreground="green", bg="black", borderwidth=0)
-label3.config(font=("handjet", 25))
-label3.place(x=35, y=210)
-
-label4 = tk.Label(foreground="green", bg="black", borderwidth=0)
-label4.config(font=("handjet", 15))
-label4.place(x=35, y=260)
+def on_focusout1(event):
+    if entry1.get() == '':
+        entry1.insert(0, 'Enter your key or generate one...')
+        entry1.config(fg = 'grey')
 
 
+root.title("Encrypto++")
+root.geometry("700x300")
+root.resizable(False, False)
+icon = PhotoImage(file='Img/icon.png')
+root.iconphoto(False, icon)
 
-def button_clicked():
-    generate_key()
+image = PhotoImage(file='Img/label.png') 
+image2 = PhotoImage(file='Img/dice.png')
+image3 = PhotoImage(file='Img/folder.png')
+image4 = PhotoImage(file='Img/file.png')
 
-button = tk.Button(text="Generate key", borderwidth=0, bg="black", fg="green", activebackground='#2e2e2e', activeforeground="green", command=button_clicked)
-button.place(x=35, y=180)
+label = tk.Label(text="Encrypto++", borderwidth=0)
+label.config(font=("handjet", 40))
+label.place(x=10, y=0)
+
+label2 = Label(root, image = image)
+label2.place(x=450,y=10)
 
 
+entry.config(fg='grey')
+entry.insert(0, 'Enter your key or generate one...')
+entry.bind('<FocusIn>', on_entry_click)
+entry.bind('<FocusOut>', on_focusout)
+entry.place(x=25, y=80)
 
+entry1.config(fg='grey')
+entry1.insert(0, 'Select a path or a directory')
+entry1.bind('<FocusIn>', on_entry_click1)  # Add this line to bind the FocusIn event
+entry1.bind('<FocusOut>', on_focusout1)  # Add this line to bind the FocusOut event
+entry1.place(x=25, y=170)
+entry1.place(x=25, y=170)
 
+button = Button(root, image=image2, command=generate_key)
+button.place(x=395, y=80)
+
+button1 = Button(root, text='Load Key',command=Load_key)
+button1.place(x=25, y=108)
+
+button2 = Button(root, text='Save Key',command=save_key)
+button2.place(x=111, y=108)
+
+button3 = Button(root, image=image3, command=folder_path)
+button3.place(x=25, y=198)
+
+button4 = Button(root, image=image4, command=file_path)
+button4.place(x=83, y=198)
+
+button5 = Button(root, command=encrypt, text='Encrypt')
+button5.place(x=315, y=198)
+
+button6 = Button(root, command=decrypt, text='Decrypt')
+button6.place(x=315, y=233)
 
 root.mainloop()
+#GITHUB:P1xelcyber
